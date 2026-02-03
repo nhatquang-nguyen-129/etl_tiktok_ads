32847e523cc17048eef6bd8c4f2b50d358a0ca9a
Nhat Quang Nguyen <nhatquang.nguyen.129@gmail.com>
2026-02-03 10:45:35 +0700

    use loader.load instead of direct call

---
+++ etl/load_ad_creative.py
@@ -0,0 +1,56 @@
+import sys
+from pathlib import Path
+ROOT_FOLDER_LOCATION = Path(__file__).resolve().parents[2]
+sys.path.append(str(ROOT_FOLDER_LOCATION))
+
+import logging
+import pandas as pd
+
+from plugins.google_bigquery import internalGoogleBigqueryLoader
+
+def load_ad_creative(
+    *,
+    df: pd.DataFrame,
+    direction: str,
+) -> None:
+    """
+    Load TikTok Ads ad creative
+    ---------
+    Workflow:
+        1. Validate input DataFrame
+        2. Validate output direction for Google BigQuery
+        3. Set primary key(s) to advertiser_id and ad_id
+        4. Use UPSERT mode with temporary table for deduplication
+        5. Make internalGoogleBigQueryLoader API call
+    ---------
+    Returns:
+        None
+    """      
+
+    if df.empty:
+        msg = ("⚠️ [LOADER] Empty TikTok Ads ad creative Dataframe then loading will be suspended.")
+        print(msg)
+        logging.warning(msg)
+        return
+
+    msg = (
+        "🔄 [LOADER] Triggering to load "
+        f"{len(df)} row(s) of TikTok Ads ad creative to Google BigQuery table "
+        f"{direction}..."
+    )
+    
+    loader = internalGoogleBigqueryLoader()
+
+    loader.load(
+        df=df,
+        direction=direction,
+        mode="upsert",
+        keys=[
+            "advertiser_id", 
+            "ad_id"
+        ],
+        partition=None,
+        cluster=[
+            "ad_id"
+        ],
+    )
