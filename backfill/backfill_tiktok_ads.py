12d8c9e73c03a074e6d631cdc26beee3c74a2b10
Nhat Quang Nguyen <nhatquang.nguyen.129@gmail.com>
2026-03-06 14:27:28 +0700

    added CLI parser argument

--- backfill/backfill_tiktok_ads.py
+++ backfill/backfill_tiktok_ads.py
@@ -23,7 +23,9 @@ if not all([
     DEPARTMENT,
     ACCOUNT,
 ]):
-    raise EnvironmentError("❌ [BACKFILL] Failed to execute TikTok Ads backfill due to missing required environment variables.")
+    raise EnvironmentError(
+        "❌ [BACKFILL] Failed to execute TikTok Ads backfill due to missing required environment variables."
+    )
 
 def backfill():
     """
@@ -63,10 +65,14 @@ def backfill():
         start_date = datetime.strptime(args.start_date, "%Y-%m-%d").strftime("%Y-%m-%d")
         end_date = datetime.strptime(args.end_date, "%Y-%m-%d").strftime("%Y-%m-%d")
     except ValueError:
-        raise ValueError("❌ [BACKFILL] Failed to execute TikTok Ads backfill due to start_date and end_date must be in YYYY-MM-DD format.")
+        raise ValueError(
+            "❌ [BACKFILL] Failed to execute TikTok Ads backfill due to start_date and end_date must be in YYYY-MM-DD format."
+        )
 
     if start_date > end_date:
-        raise ValueError("❌ [BACKFILL] Failed to execute TikTok Ads backfill due to start_date must be less than or equal to end_date.")
+        raise ValueError(
+            "❌ [BACKFILL] Failed to execute TikTok Ads backfill due to start_date must be less than or equal to end_date."
+        )
 
     print(
         "🔄 [BACKFILL] Triggering to execute TikTok Ads backfill for "
@@ -80,7 +86,9 @@ def backfill():
 
 # Initialize Google Secret Manager
     try:
-        print("🔍 [BACKFILL] Initialize Google Secret Manager client...")
+        print(
+            "🔍 [BACKFILL] Initialize Google Secret Manager client..."
+        )
 
         google_secret_client = secretmanager.SecretManagerServiceClient(
             client_options=ClientOptions(
@@ -88,7 +96,9 @@ def backfill():
             )
         )
 
-        print("✅ [BACKFILL] Successfully initialized Google Secret Manager client.")
+        print(
+            "✅ [BACKFILL] Successfully initialized Google Secret Manager client."
+        )
     
     except Exception as e:
         raise RuntimeError(
