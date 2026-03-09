4290cb849abe953949e47bcf9f8fba9aaa6cf2a2
Nhat Quang Nguyen <nhatquang.nguyen.129@gmail.com>
2026-03-09 16:00:16 +0700

    set mode to upsert

--- etl/load_ad_insights.py
+++ etl/load_ad_insights.py
@@ -14,14 +14,14 @@ def load_ad_insights(
 ) -> None:
     """
     Load TikTok Ads ad insights
-    ----------------------
-    Workflow:
+    ---
+    Principles:
         1. Validate input DataFrame
         2. Validate output direction for Google BigQuery
         3. Set primary key(s) to date
         4. Use UPSERT mode with parameterized query for deduplication
         5. Make internalGoogleBigQueryLoader API call
-    ---------
+    ---
     Returns:
         None
     """    
