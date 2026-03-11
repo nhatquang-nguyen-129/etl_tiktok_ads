32f56814fba36063e33a9df9a1a2593bf989d8be
Nhat Quang Nguyen <nhatquang.nguyen.129@gmail.com>
2026-03-11 09:08:05 +0700

    changed cluster to video_id

--- etl/load_ad_creative.py
+++ etl/load_ad_creative.py
@@ -27,9 +27,11 @@ def load_ad_creative(
     """      
 
     if df.empty:
+        
         print(
             "⚠️ [LOADER] Empty TikTok Ads ad creative then loading will be suspended."
         )
+        
         return
 
     print(
